dc9c079b2cac71423c43fc0286d405af4f28832f
Alyssa Green <18arg5@queensu.ca>
2023-02-09 15:13:28 -0500

    script to calculate p-values

---
+++ pcadapt_script.R
@@ -0,0 +1,26 @@
+####################
+# Run pcadapt scan #
+####################
+#
+#       args[1]: zscores matrix
+#       args[2]: prefix for output files
+#
+
+args = commandArgs(trailingOnly=TRUE)
+
+library(RcppCNPy)
+library(bigutilsr)
+
+
+zscores <- Zscores
+K <- ncol(zscores)
+
+# For one component only
+if (K == 1) {
+        d2 <- (zscores - median(zscores))^2
+} else {
+        d2 <- dist_ogk(zscores)
+}
+
+write.table(d2, file=paste0(args[2], ".pcadapt.test.txt"), quote=F, row.names=F, col.names=F)
+write.table(pchisq(d2, df=K, lower.tail=F), file="test.pcadapt.pval.txt", quote=F, row.names=F, col.names=F)
